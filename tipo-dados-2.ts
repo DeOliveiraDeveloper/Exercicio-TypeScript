40704661678a0e0381e8905f80354cca0d16de41
DeOliveiraDeveloper <monteirocaio4@gmail.com>
2024-04-18 11:32:33 -0300

    first commit

---
+++ tipo-dados-2.ts
@@ -0,0 +1,7 @@
+let nomeGuerra: string = "Matador";
+let grad: string = "Cb";
+let tempoServico: number = 20;
+
+let tudao = `O ${grad} ${nomeGuerra} tem mais de ${tempoServico} anos de serviço.`
+
+console.log(tudao);
---
+++ tipos-dados.js
@@ -0,0 +1,3 @@
+let nome = 'Caio';
+console.log(nome);
+console.log(typeof nome);
